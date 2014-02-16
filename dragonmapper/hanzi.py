3e6b40f28accad8ae8bdf66b30e0f4ff5d446971
Thomas Roten <thomas@roten.us>
2014-02-16 08:52:14 +0700

    Renames variables.

--- dragonmapper/hanzi.py
+++ dragonmapper/hanzi.py
@@ -126,23 +126,23 @@ def is_simplified(s):
     return identify(s) in (SIMPLIFIED, BOTH)
 
 
-def _hanzi_to_pinyin(w):
+def _hanzi_to_pinyin(hanzi):
     """Return the Pinyin reading for a Chinese word.
 
-    If the given string *w* matches a CC-CEDICT word, the return value is
+    If the given string *hanzi* matches a CC-CEDICT word, the return value is
     formatted like this: [WORD_READING1, WORD_READING2, ...]
 
-    If the given string *w* doesn't match a CC-CEDICT word, the return value
-    is formatted like this: [[CHAR_READING1, CHAR_READING2 ...], ...]
+    If the given string *hanzi* doesn't match a CC-CEDICT word, the return
+    value is formatted like this: [[CHAR_READING1, CHAR_READING2 ...], ...]
 
     When returning character readings, if a character wasn't recognized, the
     original character is returned, e.g. [[CHAR_READING1, ...], CHAR, ...]
 
     """
     try:
-        return _HANZI_PINYIN_MAP['words'][w]
+        return _HANZI_PINYIN_MAP['words'][hanzi]
     except KeyError:
-        return [_CHARACTERS.get(c, c) for c in w]
+        return [_CHARACTERS.get(c, c) for c in hanzi]
 
 
 def to_pinyin(hanzi, delimiter=' ', all_readings=False, accented=True):
