872fab5930bcb69d9801f20616acfeaf593d57f3
Thomas Roten <thomas@roten.us>
2014-04-14 14:26:24 +0800

    Bumps version to 0.2dev.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 
 setup(
     name='dragonmapper',
-    version='0.1',
+    version='0.2dev',
     author='Thomas Roten',
     author_email='thomas@roten.us',
     url='https://github.com/tsroten/dragonmapper',
