0398d095a92f1ba173b380d0a1c29d8a64d723a5
Thomas Roten <thomas@roten.us>
2014-04-28 08:10:47 +0800

    Adds setup publish command.

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
 
 with open('README.rst') as f:
     long_description = f.read()
