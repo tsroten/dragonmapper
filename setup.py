f57b5b285f01962b6bf26f3ecdd1175a2d0f2da1
Thomas Roten <thomas@roten.us>
2014-04-28 08:10:19 +0800

    Adds fallback to distutils if setuptools isn't installed.

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 with open('README.rst') as f:
     long_description = f.read()
