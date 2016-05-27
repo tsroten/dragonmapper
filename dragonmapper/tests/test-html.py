ab6e93fe751a92abf89699a5469d8eaac133c2f9
Tait Hoyem <taitskywalker@gmail.com>
2016-05-26 22:47:47 -0600

    Updated tests to include internal functions

--- dragonmapper/tests/test-html.py
+++ dragonmapper/tests/test-html.py
@@ -10,11 +10,17 @@ from dragonmapper import html
 
 class TestHtmlFuctions(unittest.TestCase):
 
+    maxDiff = None
+
     f = codecs.open("dragonmapper/data/test-html-data.txt", 'r', 'utf8')
 
     s = '我叫顏毅'
-    zh = ['ㄨㄛˇ', 'ㄐㄧㄠˋ', 'ㄧㄢˊ', 'ㄧˋ']
-    pi = ['wǒ', 'jiào', 'yán', 'yì']
+    zh = 'ㄨㄛˇ ㄐㄧㄠˋ ㄧㄢˊ ㄧˋ'
+    pi = 'wǒ jiào yán yì'
+
+    s2 = '你好，我媽媽對我叫“顏毅”。'
+    zh2 = 'ㄋㄧˇ ㄏㄠˇ，ㄨㄛˇ ㄇㄚ ㄇㄚ ㄉㄨㄟˋ ㄨㄛˇ ㄐㄧㄠˋ：“ㄧㄢˊ ㄧˋ”'
+    pi2 = 'nǐ hǎo，wǒ mā mā duì wǒ jiào：“yán yì”'
 
     indented_5 = f.readline()\
         .replace("\\n", "\n").replace("\\t", "\t").rstrip('\n')
@@ -71,3 +77,31 @@ class TestHtmlFuctions(unittest.TestCase):
             html.to_html(
                 self.s, bottom=self.pi),
             self.pinyin_bottom)
+
+    def test_identify(self):
+        self.assertEqual(html._identify("，"), 'punct')
+        self.assertEqual(html._identify("你好嗎？"), 'hanzi')
+        self.assertEqual(html._identify("wǒ mā mā"), 'pinyin')
+        self.assertEqual(html._identify("ㄨㄛˇ ㄐㄧㄠˋ ㄧㄢˊ ㄧˋ"), 'zhuyin')
+        self.assertEqual(html._identify("1"), 'tone-mark')
+        self.assertEqual(html._identify("ˋ"), 'tone-mark')
+        self.assertEqual(html._identify("："), 'punct')
+
+    def test_stackify(self):
+        self.assertEqual(html._stackify("ni3"), "n<br />i<br />3<br />")
+        self.assertEqual(html._stackify("ㄨㄛˇ"), "ㄨ<br />ㄛ<br />ˇ<br />")
+        self.assertEqual(html._stackify("小狗"), "小<br />狗<br />")
+        self.assertEqual(html._stackify("phantom"),
+            "p<br />h<br />a<br />n<br />t<br />o<br />m<br />")
+        self.assertEqual(html._stackify("gxF52f"),
+            "g<br />x<br />F<br />5<br />2<br />f<br />")
+
+    def test_split_punct(self):
+        self.assertEqual(html._split_punct("你好嗎？"), ['你', '好', '嗎', ''])
+        self.assertEqual(html._split_punct("ㄨㄛˇ：ㄇㄚ ㄇㄚ"),
+            ['ㄨㄛˇ', '', 'ㄇㄚ', 'ㄇㄚ'])
+        self.assertEqual(html._split_punct("我叫：“顏毅”"),
+            ['我', '叫', '', '', '顏', '毅', ''])
+        self.assertEqual(html._split_punct(
+            "ni3 shi4：wo3 de5 peng2 you5 ma5？",
+            ['ni3', 'shi4', '', 'wo3', 'de5', 'peng2', 'you5', 'ma', ''])
