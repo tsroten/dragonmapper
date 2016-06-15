348e283c46d53fb94e6b00d97617fb14c967e63c
Tait Hoyem <taitskywalker@gmail.com>
2016-06-14 19:09:16 -0600

    Added Jyutping tests

--- dragonmapper/tests/test-hanzi.py
+++ dragonmapper/tests/test-hanzi.py
@@ -21,6 +21,15 @@ class TestConversionFunctions(unittest.TestCase):
     npinyin_segmented_readings = '[ai4] [xi3huan5] [ai4]。'
     ipa = 'aɪ˥˩ ɕi˧˩˧ xwan aɪ˥˩。'
     zhuyin = 'ㄞˋ ㄒㄧˇ ㄏㄨㄢ˙ ㄞˋ。'
+    jyutping = 'oi3hei2fun1oi3。'
+    jyutping_segmented = 'oi3 hei2fun1 oi3。'
+
+    def test_jyutping(self):
+        self.assertEqual(hanzi.to_jyutping(self.chinese), self.jyutping)
+        self.assertEqual(
+            hanzi.to_jyutping(
+                self.chinese_segmented),
+            self.jyutping_segmented)
 
     def test_accented_pinyin(self):
         self.assertEqual(hanzi.to_pinyin(self.chinese), self.apinyin)
